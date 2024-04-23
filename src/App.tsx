d3705263067c3600325e1ebd495189bc8220d31a
탁민주 <takminjoo@hanssem.com>
2024-04-23 10:47:28 +0900

    husky 테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback } from 'react';
 
-console.log('App.tsx 123');
+console.log('App.tsx');
 
 function App() {
   return <div>min_99의 boilerplate 프로젝트 입니다.</div>;
